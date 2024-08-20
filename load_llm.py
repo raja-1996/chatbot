f449a00dfdc3ec800fd11ef8cea0e2be1b3fa9ed
raja-1996 <rajamohanrdy@gmail.com>
2024-08-20 19:50:11 +0530

    Update load_llm.py

--- load_llm.py
+++ load_llm.py
@@ -3,16 +3,8 @@ import os
 import groq
 
 
-os.environ["LANGSMITH_API_KEY"] = "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"
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_PROJECT"] = "Meesho Chatbot"
-os.environ["USER_AGENT"] = "Mozilla/5.0"
-
-os.environ["TAVILY_API_KEY"] = "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"
-
 from groq import Groq
 
-os.environ["GROQ_API_KEY"] = "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"
 
 
 def load_groq_client():
